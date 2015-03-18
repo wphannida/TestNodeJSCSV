9cf5a96f999d6a4260e9f406564ccfd5847a98fa
Phannida W <phannida.wang@thomsonreuters.com>
2015-03-18 11:12:34 +0700

    Write data which read from csv to txt file in json format

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ var csv = require('ya-csv');
 
 //load csv file
 var loadCsv = function() {
-    var reader = csv.createCsvFileReader('CSRPRAW.csv', {
+    var reader = csv.createCsvFileReader('CSRPRAW_Sample.csv', {
         'separator': ',',
         'quote': '"',
         'escape': '"',
         'comment': ''   });
     var allEntries = new Array();
-
+    var fs = require('fs');
    // console.log('reader==> ',reader);
     //reader.setColumnNames([ 'firstName', 'lastName', 'username' ]);
     reader.columnsFromHeader =true;
@@ -20,6 +20,37 @@ var loadCsv = function() {
         allEntries.push(data);
         //writer.writeRecord([ data[0] ]);
         console.log('allEntries==> ',allEntries);
+
+
+//        fs.writeFile("Tmp/test.txt",data, function(err) {
+//            if(err) {
+//                return console.log(err);
+//            }
+//
+//            console.log("The file was saved!");
+//        });
+
+        fs.writeFile("Tmp/test.txt", JSON.stringify(allEntries), function(err) {
+
+            if(err) {
+                return console.log(err);
+            }
+
+            console.log("The file was saved!");
+
+        });
+
+
+
+//        var stream = fs.createWriteStream("Tmp/test.txt");
+//        stream.once('open', function(fd) {
+//            stream.write("My first row\n");
+//            stream.write("My second row\n");
+//            stream.end();
+//        });
+
+
+
 
     });
 
--- web.js
+++ web.js
@@ -10,6 +10,18 @@ function process_request(req, res) {
         'Content-Type': 'text/plain'
     });
     res.end(body);
+
+    var fs = require('fs');
+    fs.writeFile("Tmp/test.txt", "Hey there!", function(err) {
+        if(err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    });
+
 }
 var s = http.createServer(process_request);
 s.listen(8080);
+
+
